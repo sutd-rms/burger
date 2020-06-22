3d6eefebe9ff14b486e5632e1660a4d8e250655e
eugenechia95 <eugenechia95@gmail.com>
2020-06-22 20:32:33 +0800

    Done with editing feature

--- src/Components/ProjectPage/ProjectOverview.js
+++ src/Components/ProjectPage/ProjectOverview.js
@@ -13,6 +13,8 @@ import InputBase from '@material-ui/core/InputBase';
 import InputLabel from '@material-ui/core/InputLabel';
 import CardMedia from '@material-ui/core/CardMedia';
 import Avatar from '@material-ui/core/Avatar';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import CancelIcon from '@material-ui/icons/Cancel';
 
 const styles = theme => ({
   root: {
@@ -23,14 +25,31 @@ const styles = theme => ({
     color: 'rgba(0, 0, 0, 0.87) !important'
   },
 
+  enabled: {
+    color: '#B4B5B7'
+  },
+
   media: {
     height: 200,
     width: 200,
-    margin: 'auto'
+    margin: 'auto',
+    marginBottom: 15
   },
 
   label: {
     color: 'black'
+  },
+
+  profilebar: {
+    textAlign: 'center'
+  },
+
+  checkIcon: {
+    color: 'green'
+  },
+
+  cancelIcon: {
+    color: 'red'
   }
 });
 
@@ -42,9 +61,7 @@ const FormInput = withStyles(theme => ({
   },
 
   input: {
-    color: 'black',
     position: 'relative',
-    backgroundColor: theme.palette.common.white,
     border: '1px solid #ced4da',
     width: '100%',
     padding: '10px 12px',
@@ -62,12 +79,15 @@ class ProjectOverview extends React.Component {
       description: 'This is a sample description!',
       cover: 'https://source.unsplash.com/random',
       titleNew: "McDonald's Australia",
-      description: 'This is a sample description!',
-      cover: 'https://source.unsplash.com/random',
+      descriptionNew: 'This is a sample description!',
+      coverNew: 'https://source.unsplash.com/random',
       editable: false
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInputChange(event) {
@@ -80,19 +100,60 @@ class ProjectOverview extends React.Component {
     });
   }
 
+  handleEdit(event) {
+    this.setState({
+      editable: true
+    });
+  }
+
+  handleCancel(event) {
+    this.setState({
+      titleNew: this.state.title,
+      descriptionNew: this.state.description,
+      coverNew: this.state.cover,
+      editable: false
+    });
+  }
+
+  handleSubmit(event) {
+    this.setState({
+      title: this.state.titleNew,
+      description: this.state.descriptionNew,
+      cover: this.state.coverNew,
+      editable: false
+    });
+  }
+
   render() {
     const { classes } = this.props;
 
     return (
       <Box boxShadow={3} px={5} py={5}>
         <Grid container spacing={3}>
-          <Grid item md={3}>
+          <Grid item md={3} className={classes.profilebar}>
             <Avatar
               alt="Remy Sharp"
               src={this.state.cover}
               className={classes.media}
             />
-            <Button color="primary">Edit Project</Button>
+            <Button
+              variant="outlined"
+              size="small"
+              disabled={this.state.editable ? true : false}
+              onClick={this.handleEdit}
+            >
+              Edit Project
+            </Button>
+            <Box mt={1} visibility={this.state.editable ? 'visible' : 'hidden'}>
+              <CheckCircleIcon
+                className={classes.checkIcon}
+                onClick={this.handleSubmit}
+              />
+              <CancelIcon
+                className={classes.cancelIcon}
+                onClick={this.handleCancel}
+              />
+            </Box>
           </Grid>
           <Grid item md={9}>
             <div>
@@ -106,10 +167,14 @@ class ProjectOverview extends React.Component {
                 </InputLabel>
                 <FormInput
                   id="bootstrap-input"
-                  disabled="true"
-                  className={classes.disabled}
-                  name="title"
-                  value={this.state.title}
+                  disabled={this.state.editable ? false : true}
+                  className={
+                    this.state.editable ? classes.enabled : classes.disabled
+                  }
+                  name={this.state.editable ? 'titleNew' : 'title'}
+                  value={
+                    this.state.editable ? this.state.titleNew : this.state.title
+                  }
                   onChange={this.handleInputChange}
                 />
               </FormControl>
@@ -125,12 +190,18 @@ class ProjectOverview extends React.Component {
                 </InputLabel>
                 <FormInput
                   multiline
-                  rows="6"
+                  rows="8"
                   id="bootstrap-input"
-                  disabled="true"
-                  className={classes.disabled}
-                  name="description"
-                  value={this.state.description}
+                  disabled={this.state.editable ? false : true}
+                  className={
+                    this.state.editable ? classes.enabled : classes.disabled
+                  }
+                  name={this.state.editable ? 'descriptionNew' : 'description'}
+                  value={
+                    this.state.editable
+                      ? this.state.descriptionNew
+                      : this.state.description
+                  }
                   onChange={this.handleInputChange}
                 />
               </FormControl>
