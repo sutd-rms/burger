401f9e12e23a0b6b34fe1baf8bb1815b27aba2c1
eugenechia95 <eugenechia95@gmail.com>
2020-06-23 15:18:43 +0800

    done with train models tab

--- src/Components/ProjectPage/TabComponents/ModelTrainingTab.js
+++ src/Components/ProjectPage/TabComponents/ModelTrainingTab.js
@@ -1,170 +1,51 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
-import FormControl from '@material-ui/core/FormControl';
-import InputBase from '@material-ui/core/InputBase';
-import InputLabel from '@material-ui/core/InputLabel';
-import Avatar from '@material-ui/core/Avatar';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import CancelIcon from '@material-ui/icons/Cancel';
-import Dropzone from 'react-dropzone';
-import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
+import { Typography } from '@material-ui/core';
+import TrainedModelsTable from './TrainedModelsTable';
+import TrainModelModal from './TrainModelModal';
 
 const styles = theme => ({
-  root: {
-    width: '90%'
-  },
-
-  disabled: {
-    color: 'rgba(0, 0, 0, 0.87) !important'
-  },
-
-  enabled: {
-    color: '#B4B5B7'
-  },
-
-  media: {
-    height: 200,
-    width: 200,
-    margin: 'auto',
-    marginBottom: 20
-  },
-
-  mediaEditable: {
-    height: 200,
-    width: 200,
-    margin: 'auto',
-    marginBottom: 20,
-    opacity: 0.5,
-    filter: 'brightness(50%)'
-  },
-
-  label: {
-    color: 'black'
-  },
-
-  profilebar: {
-    textAlign: 'center'
-  },
-
-  checkIcon: {
-    color: 'green',
-    marginRight: 10
-  },
-
-  cancelIcon: {
-    color: 'red',
-    marginLeft: 10
-  },
-  card: {
-    position: 'relative'
-  },
-  overlayEditable: {
-    position: 'absolute',
-    top: '33%',
-    left: '33%',
-    color: 'white'
-  },
-  hidden: {
-    display: 'none'
-  },
-  photoIcon: {
-    width: '50px',
-    height: '50px'
-  }
+  root: {}
 });
-
-const FormInput = withStyles(theme => ({
-  root: {
-    'label + &': {
-      marginTop: theme.spacing(3)
-    }
-  },
-
-  input: {
-    position: 'relative',
-    border: '1px solid #ced4da',
-    width: '100%',
-    padding: '10px 12px',
-    marginBottom: '20px',
-    transition: theme.transitions.create(['border-color', 'box-shadow'])
-  }
-}))(InputBase);
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-class ProjectOverview extends React.Component {
+class ModelTrainingTab extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: '',
-      title: "McDonald's Australia",
-      description: 'This is a sample description!',
-      cover: 'https://source.unsplash.com/random',
-      titleNew: "McDonald's Australia",
-      descriptionNew: 'This is a sample description!',
-      coverNew: 'https://source.unsplash.com/random',
-      editable: false,
+      open: false,
       success: false
     };
 
-    this.onDrop = files => {
-      var acceptedFiles = files.map(file =>
-        Object.assign(file, {
-          preview: URL.createObjectURL(file)
-        })
-      );
-      this.setState({
-        coverNew: acceptedFiles[0].preview
-      });
-    };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
-    this.handleCancel = this.handleCancel.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+    this.showAlert = this.showAlert.bind(this);
     this.handleCloseSnackbar = this.handleCloseSnackbar.bind(this);
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-
+  handleOpenModal(event) {
     this.setState({
-      [name]: value
+      open: true
     });
   }
 
-  handleEdit(event) {
+  handleCloseModal(event) {
     this.setState({
-      editable: true
+      open: false
     });
   }
 
-  handleCancel(event) {
-    this.setState({
-      titleNew: this.state.title,
-      descriptionNew: this.state.description,
-      coverNew: this.state.cover,
-      editable: false
-    });
-  }
-
-  handleSubmit(event) {
+  showAlert(event) {
     //Make POST request here
 
     this.setState({
-      title: this.state.titleNew,
-      description: this.state.descriptionNew,
-      cover: this.state.coverNew,
-      editable: false,
       success: true
     });
   }
@@ -180,138 +61,32 @@ class ProjectOverview extends React.Component {
 
     return (
       <Box boxShadow={3} px={5} py={5}>
-        <Grid container spacing={3}>
-          <Grid item md={3} className={classes.profilebar}>
-            <Dropzone
-              onDrop={this.onDrop}
-              disabled={!this.state.editable}
-              multiple={false}
-              accept="image/jpeg, image/png, image/jpg"
-            >
-              {({ getRootProps, getInputProps }) => (
-                <section
-                  className="container"
-                  className={classes.dropContainer}
-                >
-                  <div {...getRootProps({ className: 'dropzone' })}>
-                    <input {...getInputProps()} />
-                    <div className={classes.card}>
-                      <Avatar
-                        alt="Profile Picture"
-                        src={
-                          this.state.editable
-                            ? this.state.coverNew
-                            : this.state.cover
-                        }
-                        className={
-                          this.state.editable
-                            ? classes.mediaEditable
-                            : classes.media
-                        }
-                      />
-                      <div
-                        className={
-                          this.state.editable
-                            ? classes.overlayEditable
-                            : classes.hidden
-                        }
-                      >
-                        <PhotoCameraIcon
-                          className={
-                            this.state.editable ? classes.photoIcon : ''
-                          }
-                        />
-                        <div>Change Image</div>
-                      </div>
-                    </div>
-                  </div>
-                </section>
-              )}
-            </Dropzone>
-            <Button
-              variant="outlined"
-              size="small"
-              disabled={this.state.editable ? true : false}
-              onClick={this.handleEdit}
-            >
-              Edit Project
-            </Button>
-            <Box mt={1} visibility={this.state.editable ? 'visible' : 'hidden'}>
-              <CheckCircleIcon
-                className={classes.checkIcon}
-                onClick={this.handleSubmit}
-              />
-              <CancelIcon
-                className={classes.cancelIcon}
-                onClick={this.handleCancel}
-              />
-            </Box>
-          </Grid>
-          <Grid item md={9}>
-            <div>
-              <FormControl className={classes.root}>
-                <InputLabel
-                  shrink
-                  htmlFor="title-input"
-                  className={classes.label}
-                >
-                  Title
-                </InputLabel>
-                <FormInput
-                  id="title-input"
-                  disabled={this.state.editable ? false : true}
-                  className={
-                    this.state.editable ? classes.enabled : classes.disabled
-                  }
-                  name={this.state.editable ? 'titleNew' : 'title'}
-                  value={
-                    this.state.editable ? this.state.titleNew : this.state.title
-                  }
-                  onChange={this.handleInputChange}
-                />
-              </FormControl>
-            </div>
-            <div>
-              <FormControl className={classes.root}>
-                <InputLabel
-                  shrink
-                  htmlFor="description-desc"
-                  className={classes.label}
-                >
-                  Description
-                </InputLabel>
-                <FormInput
-                  multiline
-                  rows="8"
-                  id="description-input"
-                  disabled={this.state.editable ? false : true}
-                  className={
-                    this.state.editable ? classes.enabled : classes.disabled
-                  }
-                  name={this.state.editable ? 'descriptionNew' : 'description'}
-                  value={
-                    this.state.editable
-                      ? this.state.descriptionNew
-                      : this.state.description
-                  }
-                  onChange={this.handleInputChange}
-                />
-              </FormControl>
-            </div>
-          </Grid>
-        </Grid>
+        <Box mb={5} px={1} display="flex" justifyContent="space-between">
+          <Typography variant="h6" component="h2">
+            TRAINED MODELS:
+          </Typography>
+          <Button variant="outlined" onClick={this.handleOpenModal}>
+            Train New Model
+          </Button>
+        </Box>
+        <TrainedModelsTable />
         <Snackbar
           open={this.state.success}
           autoHideDuration={6000}
           onClose={this.handleCloseSnackbar}
         >
           <Alert onClose={this.handleCloseSnackbar} severity="success">
-            Project Edited!
+            Model training has started! Please check again later!
           </Alert>
         </Snackbar>
+        <TrainModelModal
+          open={this.state.open}
+          handleClose={this.handleCloseModal}
+          showAlert={this.showAlert}
+        />
       </Box>
     );
   }
 }
 
-export default withStyles(styles, { withTheme: true })(ProjectOverview);
+export default withStyles(styles, { withTheme: true })(ModelTrainingTab);
---
+++ src/Components/ProjectPage/TabComponents/TrainModelModal.js
@@ -0,0 +1,269 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(8, 15, 8)
+  },
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(5)
+  },
+  backButton: {
+    marginRight: theme.spacing(1)
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+    fontWeight: 'bold'
+  },
+  actionButtons: {
+    marginTop: theme.spacing(5),
+    textAlign: 'right'
+  },
+  submitSection: {
+    marginTop: theme.spacing(5),
+    textAlign: 'right'
+  },
+  submitButton: {
+    backgroundColor: 'green',
+    color: 'white'
+  },
+  title: {
+    fontWeight: 'bold'
+  }
+});
+
+function getSteps() {
+  return ['Model Selection', 'Dataset Selection', 'Name Selection'];
+}
+
+class ModelTrainingTab extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeStep: 0,
+      modelList: ['Default Model', 'Neural Network'],
+      model: 'Default Model',
+      datasetList: ['random_csv_file.csv', 'iloverms.csv', 'test.csv'],
+      dataset: 'random_csv_file.csv',
+      name: null
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleNext = this.handleNext.bind(this);
+    this.handleBack = this.handleBack.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.getStepContent = this.getStepContent.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    console.log(name);
+    console.log(value);
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleNext(event) {
+    this.setState({
+      activeStep: this.state.activeStep + 1
+    });
+  }
+
+  handleBack(event) {
+    this.setState({
+      activeStep: this.state.activeStep - 1
+    });
+  }
+
+  handleReset(event) {
+    this.setState({
+      activeStep: 0
+    });
+  }
+
+  handleSubmit(event) {
+    //Make POST request here
+    this.props.handleClose();
+    this.props.showAlert();
+    this.handleReset();
+  }
+
+  getStepContent(stepIndex) {
+    switch (stepIndex) {
+      case 0:
+        return (
+          <div>
+            <Typography variant="h6" gutterBottom>
+              Select a Model:
+            </Typography>
+            <Select
+              labelId="model"
+              id="model"
+              key="model"
+              name="model"
+              fullWidth
+              value={this.state.model}
+              onChange={this.handleInputChange}
+            >
+              {this.state.modelList.map(value => (
+                <MenuItem value={value} id={value} key={value}>
+                  {value}
+                </MenuItem>
+              ))}
+            </Select>
+          </div>
+        );
+      case 1:
+        return (
+          <div>
+            <Typography variant="h6" gutterBottom>
+              Select a Dataset to train your Model:
+            </Typography>
+            <Select
+              labelId="dataset"
+              id="dataset"
+              key="dataset"
+              name="dataset"
+              fullWidth
+              value={this.state.dataset}
+              onChange={this.handleInputChange}
+            >
+              {this.state.datasetList.map(value => (
+                <MenuItem value={value} id={value} key={value}>
+                  {value}
+                </MenuItem>
+              ))}
+            </Select>
+          </div>
+        );
+
+      case 2:
+        return (
+          <div>
+            <Typography variant="h6" gutterBottom>
+              Give it a name!:
+            </Typography>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              name="name"
+              value={this.state.name}
+              onChange={this.handleInputChange}
+              autoFocus
+            />
+          </div>
+        );
+      default:
+        return 'Unknown stepIndex';
+    }
+  }
+
+  render() {
+    const { classes } = this.props;
+    const steps = getSteps();
+
+    return (
+      <React.Fragment>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={this.props.open}
+          onClose={this.props.handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500
+          }}
+        >
+          <Fade in={this.props.open}>
+            <div className={classes.paper}>
+              <Typography variant="h5" className={classes.title}>
+                Train New Model
+              </Typography>
+              <div className={classes.root}>
+                <Stepper activeStep={this.state.activeStep} alternativeLabel>
+                  {steps.map(label => (
+                    <Step key={label}>
+                      <StepLabel>{label}</StepLabel>
+                    </Step>
+                  ))}
+                </Stepper>
+                <div>
+                  {this.state.activeStep === steps.length ? (
+                    <div className={classes.submitSection}>
+                      <Button
+                        onClick={this.handleReset}
+                        className={classes.backButton}
+                      >
+                        Reset
+                      </Button>
+                      <Button
+                        variant="contained"
+                        className={classes.submitButton}
+                        onClick={this.handleSubmit}
+                      >
+                        Start Training
+                      </Button>
+                    </div>
+                  ) : (
+                    <div>
+                      <Typography className={classes.instructions}>
+                        {this.getStepContent(this.state.activeStep)}
+                      </Typography>
+                      <div className={classes.actionButtons}>
+                        <Button
+                          disabled={this.state.activeStep === 0}
+                          onClick={this.handleBack}
+                          className={classes.backButton}
+                        >
+                          Back
+                        </Button>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          onClick={this.handleNext}
+                        >
+                          Next
+                        </Button>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </Fade>
+        </Modal>
+      </React.Fragment>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(ModelTrainingTab);
