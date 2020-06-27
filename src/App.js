c5747963ffac882ee61d72429406597f75e9116e
eugenechia95 <eugenechia95@gmail.com>
2020-06-27 23:30:11 +0800

    added in axios, redux, done with login route... shag

--- src/App.js
+++ src/App.js
@@ -14,33 +14,29 @@ import PasswordResetConfirm from './Components/PasswordResetConfirm';
 
 function App() {
   return (
-    <Router>
-      {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/logout">
-          <div>logout</div>
-        </Route>
-        <Route path="/register">
-          <div>register</div>
-        </Route>
-        <Route path="/password-reset/:uid/:token">
-          <PasswordResetConfirm />
-        </Route>
-        <Route path="/password-reset">
-          <PasswordReset />
-        </Route>
-        <Route path="/Dashboard">
-          <Dashboard />
-        </Route>
-        <Route path="/">
-          <Login />
-        </Route>
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/logout">
+        <div>logout</div>
+      </Route>
+      <Route path="/register">
+        <div>register</div>
+      </Route>
+      <Route path="/password-reset/:uid/:token">
+        <PasswordResetConfirm />
+      </Route>
+      <Route path="/password-reset">
+        <PasswordReset />
+      </Route>
+      <Route path="/Dashboard">
+        <Dashboard />
+      </Route>
+      <Route path="/">
+        <Login />
+      </Route>
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import * as serviceWorker from './serviceWorker';
+import history from './history';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router history={history}>
+        <Route path="/" component={App} />
+      </Router>
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
