77be55172f5ee38e26b90aeb7fa80d4a9465e777
eugenechia95 <eugenechia95@gmail.com>
2020-07-13 17:49:23 +0800

    Done with overall UI of constraint creation

--- src/Components/ProjectPage/TabComponents/ConstraintModal.js
+++ src/Components/ProjectPage/TabComponents/ConstraintModal.js
@@ -14,9 +14,13 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
-import ConstraintsMappingTable from './ConstraintsMappingTable';
 import InfoIcon from '@material-ui/icons/Info';
 import Tooltip from '@material-ui/core/Tooltip';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import FormControl from '@material-ui/core/FormControl';
+import InputBase from '@material-ui/core/InputBase';
+import InputLabel from '@material-ui/core/InputLabel';
+import NativeSelect from '@material-ui/core/NativeSelect';
 
 const styles = theme => ({
   modal: {
@@ -61,8 +65,24 @@ const styles = theme => ({
 });
 
 function getSteps() {
-  return ['Dataset Selection', 'Constraint Creation'];
+  return ['Dataset Selection', 'Item Selection', 'Constraint Creation'];
 }
+
+const FormInput = withStyles(theme => ({
+  root: {
+    'label + &': {
+      marginTop: theme.spacing(3)
+    }
+  },
+
+  input: {
+    position: 'relative',
+    border: '1px solid #ced4da',
+    width: '50px',
+    padding: '10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow'])
+  }
+}))(InputBase);
 
 class ConstraintModal extends React.Component {
   constructor(props) {
@@ -71,10 +91,40 @@ class ConstraintModal extends React.Component {
       activeStep: 0,
       datasetList: ['random_csv_file.csv', 'iloverms.csv', 'test.csv'],
       dataset: 'random_csv_file.csv',
-      constraints: ''
+      inequalities: ['=', '<', '<=', '>', '>='],
+      constraints: '',
+      constraints: [],
+      items: [],
+      allitems: [
+        '1102',
+        '1103',
+        '1104',
+        '1105',
+        '1106',
+        '1107',
+        '1108',
+        '3102',
+        '3106',
+        '3109',
+        '4102',
+        '5102',
+        '5602',
+        '5802',
+        '6102',
+        '7102',
+        '8102',
+        '9102',
+        '9202'
+      ]
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleConstraintsInputChange = this.handleConstraintsInputChange.bind(
+      this
+    );
+    this.handleItemsChange = this.handleItemsChange.bind(this);
+    this.createConstraintsArray = this.createConstraintsArray.bind(this);
+    this.createConstraintsForm = this.createConstraintsForm.bind(this);
     this.handleNext = this.handleNext.bind(this);
     this.handleBack = this.handleBack.bind(this);
     this.handleReset = this.handleReset.bind(this);
@@ -86,15 +136,48 @@ class ConstraintModal extends React.Component {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-    console.log(name);
-    console.log(value);
 
     this.setState({
       [name]: value
     });
   }
 
+  handleConstraintsInputChange = idx => event => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    const newConstraints = this.state.constraints.map(item => {
+      if (item.id === idx) {
+        const updatedItem = {
+          ...item,
+          [name]: value
+        };
+
+        return updatedItem;
+      }
+
+      return item;
+    });
+
+    this.setState({
+      constraints: newConstraints
+    });
+  };
+
+  handleItemsChange(event, values) {
+    if (values.length > 10) {
+      return;
+    }
+    this.setState({
+      items: values
+    });
+  }
+
   handleNext(event) {
+    if (this.state.activeStep == 1) {
+      this.createConstraintsArray(this.state.items);
+    }
     this.setState({
       activeStep: this.state.activeStep + 1
     });
@@ -119,13 +202,82 @@ class ConstraintModal extends React.Component {
     this.handleReset();
   }
 
+  createConstraintsArray(selectedItems) {
+    this.state.constraints = [];
+    selectedItems.forEach((selectedItem, idx) =>
+      this.state.constraints.push({
+        id: idx,
+        item: selectedItem,
+        coefficient: '',
+        inequality: '',
+        rhs: ''
+      })
+    );
+  }
+
+  createConstraintsForm() {
+    const listItems = this.state.constraints.map((constraint, idx) => (
+      <Box display="flex" alignItems="center" mt={5}>
+        <Box mr={5}>
+          <Typography>Item</Typography>
+          <Typography>{constraint.item}:</Typography>
+        </Box>
+        <Box mr={5}>
+          <FormControl>
+            <InputLabel shrink htmlFor="title-input">
+              Coefficient
+            </InputLabel>
+            <FormInput
+              id="title-input"
+              name="coefficient"
+              value={constraint.coefficient}
+              onChange={this.handleConstraintsInputChange(idx)}
+            />
+          </FormControl>
+        </Box>
+        <Box mr={5}>
+          <FormControl>
+            <InputLabel shrink htmlFor="title-input">
+              Inequality
+            </InputLabel>
+            <NativeSelect
+              name="inequality"
+              value={constraint.inequality}
+              input={<FormInput />}
+              onChange={this.handleConstraintsInputChange(idx)}
+            >
+              {this.state.inequalities.map(value => (
+                <option value={value}>{value}</option>
+              ))}
+            </NativeSelect>
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <InputLabel shrink htmlFor="title-input">
+              RHS
+            </InputLabel>
+            <FormInput
+              id="title-input"
+              name="rhs"
+              value={constraint.rhs}
+              onChange={this.handleConstraintsInputChange(idx)}
+            />
+          </FormControl>
+        </Box>
+      </Box>
+    ));
+
+    return listItems;
+  }
+
   getStepContent(stepIndex) {
     switch (stepIndex) {
       case 0:
         return (
           <div>
             <Typography variant="h6" gutterBottom>
-              Select a Dataset to for creating constraints:
+              Select a Dataset for creating constraints:
             </Typography>
             <Select
               labelId="dataset"
@@ -147,49 +299,48 @@ class ConstraintModal extends React.Component {
 
       case 1:
         return (
+          <Box display="flex" justifyContent="center">
+            <Box mt={5}>
+              <Box display="flex" alignItems="center">
+                <Typography variant="h6">
+                  Choose items for constraint creation
+                </Typography>
+                <Tooltip
+                  title="Items selected will be presented in the selected graph type"
+                  placement="right"
+                >
+                  <IconButton aria-label="delete">
+                    <InfoIcon />
+                  </IconButton>
+                </Tooltip>
+              </Box>
+              <Box style={{ width: 500 }}>
+                <Autocomplete
+                  multiple
+                  options={this.state.allitems}
+                  getOptionLabel={option => option}
+                  onChange={this.handleItemsChange}
+                  value={this.state.items}
+                  renderInput={params => (
+                    <TextField
+                      {...params}
+                      variant="standard"
+                      placeholder="Search Item"
+                      margin="normal"
+                      fullWidth
+                    />
+                  )}
+                />
+              </Box>
+            </Box>
+          </Box>
+        );
+
+      case 2:
+        return (
           <div>
-            <Grid container spacing={8}>
-              <Grid item md={6}>
-                <Box display="flex" alignItems="center">
-                  <Typography variant="h6">Item Mappings</Typography>
-                  <Tooltip
-                    title="Items in csv file are mapped into variables"
-                    placement="right"
-                  >
-                    <IconButton aria-label="delete">
-                      <InfoIcon />
-                    </IconButton>
-                  </Tooltip>
-                </Box>
-                <ConstraintsMappingTable />
-              </Grid>
-              <Grid item md={6}>
-                <Box>
-                  <Box display="flex" alignItems="center">
-                    <Typography variant="h6">Constraints</Typography>
-                    <Tooltip
-                      title="Input constraints in the following format: '2X1+3X2=3, 3X3=4, 4X1<5'"
-                      placement="right"
-                    >
-                      <IconButton aria-label="delete">
-                        <InfoIcon />
-                      </IconButton>
-                    </Tooltip>
-                  </Box>
-                  <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    multiline
-                    rows={10}
-                    name="constraints"
-                    value={this.state.constraints}
-                    onChange={this.handleInputChange}
-                    autoFocus
-                  />
-                </Box>
-              </Grid>
-            </Grid>
+            <Typography variant="h6">Create Constraints</Typography>
+            {this.createConstraintsForm()}
           </div>
         );
       default:
